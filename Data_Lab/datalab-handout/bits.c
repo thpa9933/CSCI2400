65b93979cff039ade9cd1f11e87fbf6f3e429fe1
Thomas Payne <thpa9933@colorado.edu>
2018-09-17 12:14:43 -0600

    DataLab 100%

--- Data_Lab/datalab-handout/bits.c
+++ Data_Lab/datalab-handout/bits.c
@@ -167,35 +167,61 @@ NOTES:
    - 285 hentaigana
    - 3 additional Zanabazar Square characters */
 /* We do not support C11 <threads.h>.  */
+
+/*DEC VARS ON TOP OF FUNCTION*/
+
+
 /* 
  * bitOr - x|y using only ~ and & 
  *   Example: bitOr(6, 5) = 7
  *   Legal ops: ~ &
  *   Max ops: 8
  *   Rating: 1
- */
 
+ De'Morgans! not(p or q) bicond (not p and not q)
+ Negate ans to account for the <not>(p or q)
+ */
 int bitOr(int x, int y) {
-  int new = x - y; 
-  return(new); 
+  int ans; 
+  ans = (~x & ~y); 
+  return ~ans; 
 } 
+
+
+/*
  * evenBits - return word with all even-numbered bits set to 1
  *   Legal ops: ! ~ & ^ | + << >>
  *   Max ops: 8
  *   Rating: 1
+  Find an even bit pattern in hex, 01010101... is 0x55 (1 byte)
+  Then left shift by 8, so you have 01010101000000000 (2 bytes)
+  If you do | between those values, the final value will be 0101010101010101
+  Then shift it over 16, which will pad right with 16 0's, use or to retain the prior value (4bytes)
  */
 int evenBits(void) {
-  return 2;
+  int a;
+  a = 0x55; 
+  a = a | a << 8;
+  a = a | a << 16; 
+  return a; 
 }
+
+
 /* 
  * minusOne - return a value of -1 
  *   Legal ops: ! ~ & ^ | + << >>
- *   Max ops: 2
+ *   Max :ops: 2
  *   Rating: 1
+ 0xff == 1111 = -1 in two's compliment
+ 0x00 = 0000, ~0x00 = 1111 = -1 in two's compliment
  */
 int minusOne(void) {
-  return 2;
+  int max;
+  max = 0x00; 
+  return ~max;
 }
+
+
 /* 
  * allEvenBits - return 1 if all even-numbered bits in word set to 1
  *   Examples allEvenBits(0xFFFFFFFE) = 0, allEvenBits(0x55555555) = 1
@@ -204,8 +230,20 @@ int minusOne(void) {
  *   Rating: 2
  */
 int allEvenBits(int x) {
-  return 2;
+  int oddBits;
+  int allBits;
+  oddBits = 0xAA;
+  oddBits = oddBits | oddBits << 8; 
+  oddBits = oddBits | oddBits << 16; // word with all odd bits
+
+  // will be a 1 at every odd bit, should be 1 at every even bit too if x has 1 at every even bit
+  allBits = oddBits | x;
+  
+  allBits = ~allBits; // should be all 0's if x had each even bit as a 1
+  return !allBits; // !000.. is 1 (! is binary (returns 1 or 0). ~ is unitary, returns 'flipped' word
 }
+
+
 /* 
  * anyOddBit - return 1 if any odd-numbered bit in word set to 1
  *   Examples anyOddBit(0x5) = 0, anyOddBit(0x7) = 1
@@ -214,8 +252,17 @@ int allEvenBits(int x) {
  *   Rating: 2
  */
 int anyOddBit(int x) {
-    return 2;
+  int y, ans;
+  y = 0xAA; 
+  y = y | y << 8; 
+  y = y | y << 16; // string of 1010
+  ans = y & x; // if x contains a 1 as an odd bit, then the result will be ex: 0010, otherwise all 0's
+  // !0010 = 0, !0 = 1;
+  // If result was 0000, !0000 = 1, !1 == 0 = false
+  // !(anything but 0000) = 0  
+  return !(!ans); 
 }
+
 
 /* 
  * byteSwap - swaps the nth byte and the mth byte
@@ -227,8 +274,42 @@ int anyOddBit(int x) {
  *  Rating: 2
  */
 int byteSwap(int x, int n, int m) {
-    return 2;
+  int new_n, new_m, mask_1, mask_2, total_mask, mask_n, mask_m, temp_mask, final_mask, ans;
+  n = n << 3; // 2^3 = 8;
+  m = m << 3; // locates where bytes are in sittingg
+
+  new_n = x >> n; // shifts the located bites to far right
+  new_m = x >> m;
+
+  // 0xff = 0000 0000 0000 .... 1111 1111
+  // 0xff & anything = make resulting string all 0's
+  new_n = 0xff & new_n; // pads evertyhing to the left of located bites with 0's 
+  new_m = 0xff & new_m; // the 0xff at the end, will allow new_n to fall through
+
+  // create new masks with same selected bit locations, with all 1's
+  mask_1 = 0xff << n; //ex: 0000 0000 1111 1111 0000 0000 ....
+  mask_2 = 0xff << m; //ex: 0000 0000 0000 0000 1111 1111 ....
+
+  // flip the bytes so located bits are 0's
+  // 1111 1111 0000 0000 1111 1111 ...
+  // you 'and' total_mask because you want 0's to be in the located positions
+  temp_mask = ~mask_1 & ~mask_2;
+  total_mask = x & temp_mask;
+ 
+  // swap the bytes, remember 'm' and 'n' are location of the bytes
+  mask_n = new_n << m;
+  mask_m = new_m << n;
+
+  // make a final word with locations swapped
+  // ex: 0000 0000 1011 0011 0000 0000 1010 1101 
+  final_mask = mask_n | mask_m;
+
+  // this allows the remaining values that were never swapped to fall through
+  ans = final_mask | total_mask; 
+  return ans; 
 }
+
+
 /* 
  * addOK - Determine if can compute x+y without overflow
  *   Example: addOK(0x80000000,0x80000000) = 0,
@@ -238,8 +319,26 @@ int byteSwap(int x, int n, int m) {
  *   Rating: 3
  */
 int addOK(int x, int y) {
-  return 2;
+  int sign_x, sign_y, sign_sum, cond;
+ // determine the sign of the ints by shifting over so only 32 position is available;
+ sign_x = x >> 31;
+ sign_y = y >> 31;
+ sign_sum = x + y;
+ sign_sum = sign_sum >> 31;
+
+ // check condtions
+ // sign_x = sign_y but sign_sum is not (always overflow) (CHECKING THIS CONDITION)
+ // sign_x = sign_y but sign_sum is (not overflow)
+ // sign_x != sign_y (never overflow)
+
+ // use ^ because it returns 1 if the are different
+ // (-x ^ y) checks if equal
+ // returns 0 if there is overflow 
+ cond = !((~sign_x ^ sign_y) & (sign_x ^ sign_sum));
+ return cond;
 }
+
+
 /* 
  * conditional - same as x ? y : z 
  *   Example: conditional(2,4,5) = 4
@@ -247,9 +346,26 @@ int addOK(int x, int y) {
  *   Max ops: 16
  *   Rating: 3
  */
+// if x evaluates to T return y
+// if x evaluates to F return z
 int conditional(int x, int y, int z) {
-  return 2;
+  int mask;
+  // ~0x00 is all 1's
+  // !x will turn x into a 1 or a 0 EX: !1011 = 0 , !0000 = 1;
+  // mask will be greater than all 1's if x > 0
+  // ~0x00 because that will be 1111 1111, where as 0xff is 0000 1111
+  mask = !x + (~0x00); // if x is 0's, then !x = 1. 1 + all 1's = 0
+  
+  // if x is 1, then 1 + all 1's overflows and becomes all 0's
+  // EX: if ~mask = 0000 ...
+     // 0000 & anything = 0000, left condition will be F, right condtion will return
+  //EX: if ~mask = 1111 ...
+     // 1111 & antyhing = anything, left condtion will be anything
+     // left condition will return because right condtion will be 0000
+  return ((~mask & z) | (mask & y));
 }
+
+
 /* 
  * isAsciiDigit - return 1 if 0x30 <= x <= 0x39 (ASCII codes for characters '0' to '9')
  *   Example: isAsciiDigit(0x35) = 1.
@@ -260,8 +376,23 @@ int conditional(int x, int y, int z) {
  *   Rating: 3
  */
 int isAsciiDigit(int x) {
-  return 2;
+  int upper, lower, upperdiff, lowerdiff;
+  // make result negative if outside of bounds
+  upper = 0x39;
+  lower = 0x30;
+
+  // set the bounds for upper and lower
+  // upperdiff equates to upper + (twos compliment of x ) + 1
+  // lowerdiff equates to x + (twos compliment of lower) + 1
+  // ~10 = -11, so you need to add 1
+  upperdiff = upper + (~x + 1); // differnce between x and the upper limit, positive if in range, negative if out of range
+  lowerdiff = x + (~lower + 1); // difference between x and the lower limit, positive if in range, negative if out or range
+ 
+  // if the sign bits are both 0, they have positive values 
+  return (!(upperdiff >> 31)) & (!(lowerdiff >> 31));
 }
+
+
 /* 
  * replaceByte(x,n,c) - Replace byte n in x with c
  *   Bytes numbered from 0 (LSB) to 3 (MSB)
@@ -272,8 +403,24 @@ int isAsciiDigit(int x) {
  *   Rating: 3
  */
 int replaceByte(int x, int n, int c) {
-  return 2;
+  int mask_ff, mask_c, mask;
+  n = n << 3;
+
+  // ex: 1111 1111 0000 0000 1111 1111 1111 ...
+  mask_ff = ~(0xff << n);
+
+  // ex: 0000 0000 cccc cccc 0000 0000 0000 ...
+  mask_c = c << n;
+ 
+  // allows our data to fall through
+  // ex: xxxx xxxx 0000 0000 xxxx xxxx xxxx ....
+  mask = x & mask_ff;
+
+  // allows day to fall through, now with the c bits
+  return mask | mask_c;
 }
+
+
 /* reverseBits - reverse the bits in a 32-bit integer,
               i.e. b0 swaps with b31, b1 with b30, etc
  *  Examples: reverseBits(0x11111111) = 0x88888888
@@ -287,8 +434,50 @@ int replaceByte(int x, int n, int c) {
  *  Rating: 4
  */
 int reverseBits(int x) {
-  return 0;
+  int mask1, mask2, mask4, mask8, mask16;
+
+  //0x55 = 01010101 01010101 01010101 01010101
+  mask1 = 0x55;
+  mask1 = mask1 | mask1 << 8;
+  mask1 = mask1 | mask1 << 16;
+  // first cond shifts all the even X bits into the odd position, where the mask is a 1
+  // those bits are then retained by being 'anded' by a 1
+
+  // second cond already has the odd X bits masked with 1's, so there values are retained
+  // the odd x bits are then shifted into the even positions
+  x = ((x >> 1) & mask1) | ((x & mask1) << 1);
+
+  //0x33 = 00110011 00110011 00110011 00110011
+  mask2 = 0x33;
+  mask2 = mask2 | mask2 << 8;
+  mask2 = mask2 | mask2 << 16;
+  x = ((x >> 2) & mask2) | ((x & mask2) << 2);
+  
+
+  //0xF = 00001111 00001111 00001111 00001111
+  mask4 = 0xF;
+  mask4 = mask4 | mask4 << 8;
+  mask4 = mask4 | mask4 << 16;
+  x = ((x >> 4) & mask4) | ((x & mask4) << 4);
+
+  // 0xFF = 0000 0000 1111 1111
+  // changing into this form after shifiting 16
+
+  // 0xFF = 00000000 11111111 00000000 11111111
+  mask8 =0xFF;
+  mask8 = mask8 | mask8 << 16;
+  x = ((x >> 8) & mask8) | ((x & mask8) << 8);
+
+  // 0xFF = 00000000 00000000 11111111 11111111
+  mask16 = 0xFF;
+  mask16 = mask16 | mask16 << 8;
+  x = ((x >> 16) & mask16) | ((x & mask16) << 16);
+
+  return x;
 }
+
+
+
 /*
  * satAdd - adds two numbers but when positive overflow occurs, returns
  *          maximum possible value, and when negative overflow occurs,
@@ -300,8 +489,43 @@ int reverseBits(int x) {
  *   Rating: 4
  */
 int satAdd(int x, int y) {
-  return 2;
+  int sign_x, sign_y, sign_sum, mask1, lowest, highest, final, sum, overflow;;
+  sign_x = x >> 31;
+  sign_y = y >> 31;
+  sum = x + y;
+  sign_sum = sum >> 31;
+  
+  //EX: sign_x = 0 (positive)
+  //EX: sign_y = 0 (positive)
+
+  // Returns 0 if there is overflow
+  overflow = !((~sign_x ^ sign_y) & (sign_x ^ sign_sum));
+  overflow = ~overflow + 1; // changes back to all 1's or all 0's
+  // EX: There IS overflow, result = 0000
+
+  // When you add two postive numbers and get a negative result its postive overflow
+
+  // Figure out if the overflow is + or -
+  // determines if x is 1 + 1111 = 0000(overflow) or 0 + 1111 (not overflow)
+  mask1 = !!sign_x; // change to 1 or 0 EX: if sign_x is 1111, !sign_x = 0, !!sign_x = 1
+  mask1 = (~mask1) + 1; // changes back to all 1's or 0's, neg val is 1 less than pos, so add 1
+  // Turns the sign of x into mask. All 1's if x is positive, and all 0's if x is negative.
+  // Want mask to be all 0's
+  // EX: There IS overflow, want ~mask = 1's in order to we return highest
+
+  // make bits to return
+  lowest = 0x1;
+  // 0x1 = 1000 0000 0000 0000
+  lowest = lowest << 31;
+  // ~0x1 = 0111 1111 1111 1111
+  highest = ~lowest;
+ 
+  // First Cond: result = 0 if there is overflow, (0 | anthing) = anything
+  final = (overflow & sum) | (~overflow & ((mask1 & lowest) | (~mask1 & highest)));
+  return final;
 }
+
+
 /*
  * Extra credit
  */
